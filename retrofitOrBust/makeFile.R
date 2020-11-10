8e91a734b7cb54b445322a249f4ef09d569537ac
Ben Anderson <dataknut@icloud.com>
2020-11-10 18:05:48 +0000

    added preamble

--- retrofitOrBust/makeFile.R
+++ retrofitOrBust/makeFile.R
@@ -1,24 +1,16 @@
 makeReport <- function(f){
   # default = html
-  rmarkdown::render(input = paste0(here::here("2020-10-16-retrofOrBust", f), ".Rmd"),
+  rmarkdown::render(input = paste0(here::here("retrofitOrBust", f), ".Rmd"),
                     params = list(title = title,
                                   subtitle = subtitle,
                                   authors = authors),
                     output_file = paste0(here::here("docs/"), f, ".html")
   )
-  # word
-  rmarkdown::render(input = paste0(here::here("2020-10-16-retrofOrBust", f), ".Rmd"),
-                    params = list(title = title,
-                                  subtitle = subtitle,
-                                  authors = authors),
-                    output_file = paste0(here::here("docs/"), f, ".docx"),
-                    output_format = "word_document"
-  )
 }
 
 # >> run report ----
 rmdFile <- "retrofitOrBust" # not the full path
-title = "Retrofit or bust?"
+title = "#backOfaFagPacket: Retrofit or bust?"
 subtitle = ""
 authors = "Ben Anderson"
 
