4406d5dc90f5a169d4701684b77732a12ff67721
Ben Anderson <dataknut@icloud.com>
2020-11-11 18:50:40 +0000

    typos

--- EPCsAndCarbon/makeFile.R
+++ EPCsAndCarbon/makeFile.R
@@ -16,14 +16,14 @@ makeReport <- function(f){
   # )
 }
 
-# >> run report ----
+# >> run EPC data check & save data ----
 rmdFile <- "epcChecks" # not the full path
 title = "Checking EPC datasets for Southampton"
 subtitle = "Data cleaning, outlier checks and coverage analysis"
 authors = "Ben Anderson"
-
 makeReport(rmdFile)
 
+# >> run the report ----
 rmdFile <- "carbonCosts" # not the full path
 title = "Exploring #backOfaFagPacket scenarios for a residential dwellings Carbon Tax"
 subtitle = "Southampton as a case study"
